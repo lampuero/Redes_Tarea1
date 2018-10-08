bc1717718ce37d526f1f9bc5cd6e70dae6c89837
Luis Ampuero <luis.ampuero@ug.uchile.cl>
2018-10-08 20:49:17 -0300

    Trabajando

--- client.py
+++ client.py
@@ -2,3 +2,13 @@ import socket
 
 
 servers = dict()
+
+def client(servers_name):
+    pass
+
+servers_data = input('Ingrese los datos de los servers en una linea\n')
+print(type(servers_data))
+servers_data_split = servers_data
+while True:
+    message =input('-> ')
+    print (message)git
