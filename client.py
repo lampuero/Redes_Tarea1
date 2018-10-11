563946d1274262a5a9e072919688192a5e2a651b
lampuero <luis.ampuero@ug.uchile.cl>
2018-10-11 10:29:50 -0300

    Agregada la funcion input con timeout

--- client.py
+++ client.py
@@ -1,5 +1,14 @@
 import socket
 import select
+import sys
+
+
+def input_with_timeout(prompt, timeout):
+    sys.stdout.write(prompt)
+    sys.stdout.flush()
+    ready, _, _ = select.select([sys.stdin], [],[], timeout)
+    if ready:
+        return sys.stdin.readline().rstrip('\n') # expect stdin to be line-buffered
 
 
 class ClientConnection:
